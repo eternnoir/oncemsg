745c8a8cb69100712355575e787d60dc00b934fa
eternnoir <eternnoir@gmail.com>
2014-09-04 14:34:07 +0800

    change hash gen logic

--- controls/messageCtrl.go
+++ controls/messageCtrl.go
@@ -1,5 +1,19 @@
 package controls
 
+import (
+	"math/rand"
+	uti "oncemsg/utility"
+	"time"
+)
+
+const layout = "2006-01-02T15:04:05.999999-07:00"
+
 func SaveMsg(msg string) (string, error) {
-	return "sss", nil
+	rand.Seed(42)
+	ha1 := uti.GetUniHashString(msg)
+	timeStr := time.Now().Format(layout)
+	ranNum := string(rand.Int31())
+	uniString := uti.GetUniHashString(ha1 + timeStr + ranNum)
+
+	return uniString, nil
 }
--- utility/utility.go
+++ utility/utility.go
@@ -1,1 +1,15 @@
 package utiltiy
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"io"
+)
+
+func GetUniHashString(content string) string {
+	h := md5.New()
+	io.WriteString(h, content)
+
+	hString := hex.EncodeToString(h.Sum(nil))
+	return hString
+}
